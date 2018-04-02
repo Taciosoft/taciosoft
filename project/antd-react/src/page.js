156ef85d836590ba8976fc7554e4fb3c19156742
Taciosoft <army.programmer@gmail.com>
2018-04-02 17:01:34 +0700

    install ant design

---
+++ project/antd-react/src/page.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import { Layout, Menu, Icon } from 'antd';
+import './page.css';
+
+
+class Page extends Component{
+render(){
+    return(
+        <div>ssssssssssssssssss</div>
+    );
+
+}
+}
+export default Page;
